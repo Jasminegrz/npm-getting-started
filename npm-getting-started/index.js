eda427ba1aa43059e1aa2111ea63d60b913d3113
Jasminegrz <114686310+Jasminegrz@users.noreply.github.com>
2023-04-11 18:21:36 +0200

    Update index.js

--- npm-getting-started/index.js
+++ npm-getting-started/index.js
@@ -1,5 +1,5 @@
 const userWilder = require('./information');
-var cowsay = require("cowsay");
+const cowsay = require("cowsay");
 console.log(cowsay.say({
     text: `Hello I'm ${userWilder.myName} from ${userWilder.campus} campus!`,
     e: "oO",
